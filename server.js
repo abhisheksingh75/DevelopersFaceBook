9e62b84eec7832c81fec83415c14b0d33ec2a460
Singh <abhisheksingh75@deloitte.com>
2020-04-28 23:52:07 +0530

    final commit

--- server.js
+++ server.js
@@ -14,6 +14,15 @@ app.use("/api/user", require("./routers/api/user"))
 app.use("/api/post", require("./routers/api/post"))
 app.use("/api/profile", require("./routers/api/profile"))
 
+//server static assesets
+if (process.env.NODE_ENV == "production") {
+  //set static folder
+  app.use(express.static("client/build"))
+  app.use("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
+
 app.listen(5000, () => {
   console.log("server listing on port" + PORT)
 })
